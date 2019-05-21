2cbdafe35bec1f15fbcb7f8c34e75f2b76c1d389
MLHeimann <heimannmarion@gmail.com>
2019-05-21 12:39:06 +0200

    Example plot.ly Sankey diagram working

--- Sankey.py
+++ Sankey.py
@@ -1,13 +1,53 @@
 import pandas as pd
+import plotly
+import plotly.graph_objs as go
+
+
 d = pd.read_csv('my_transactions.csv')
 
 # Relevant columns
-d = d.drop(["Date","Original Transaction Description","My Transaction Description", "Account", "Notes", "Pay month", "Split Transaction"], axis=1)
+# d = d.drop(["Date","Original Transaction Description","My Transaction Description", "Account", "Notes", "Pay month", "Split Transaction"], axis=1)
 
 # Creating new dataframe for income items
-isincome = d['Spending Group']=="Income"
-income = d[isincome]
+# isincome = d['Spending Group']=="Income"
+# income = d[isincome]
 
+
+# # Simple graph of transaction amounts over time
+# plotly.offline.plot({
+#     "data": [go.Scatter(x= d['Date'], y=d["Amount"])],
+#     "layout": go.Layout(title="hello world")
+# })
+
+
+# Sample Sankey example for plotly
+data = dict(
+    type='sankey',
+    node = dict(
+      pad = 15,
+      thickness = 20,
+      line = dict(
+        color = "black",
+        width = 0.5
+      ),
+      label = ["A1", "A2", "B1", "B2", "C1", "C2"],
+      color = ["blue", "blue", "blue", "blue", "blue", "blue"]
+    ),
+    link = dict(
+      source = [0,1,0,2,3,3],
+      target = [2,3,3,4,4,5],
+      value = [8,4,2,8,4,2]
+  ))
+
+layout =  dict(
+    title = "Basic Sankey Diagram",
+    font = dict(
+      size = 10
+    )
+)
+
+fig = dict(data=[data], layout=layout)
+plotly.offline.plot(fig, validate=False)
 
 
 # print(d.head())
